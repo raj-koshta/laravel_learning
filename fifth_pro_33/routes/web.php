feb8657c2768bdd1886ec19f17bf6117fe7fcce7
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-06-28 22:06:03 +0530

    Route Methods - 33

--- fifth_pro_33/routes/web.php
+++ fifth_pro_33/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', [UserController::class, 'getUser']);
+Route::view('form', [UserController::class, 'user']);
+Route::post('user', [UserController::class, 'post']);
+Route::put('user', [UserController::class, 'put']);
+Route::patch('user', [UserController::class, 'patch']);
+Route::delete('user', [UserController::class, 'delete']);

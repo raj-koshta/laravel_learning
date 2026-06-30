8d6414bcb73103e8857612073286321067eb1baa
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-06-30 23:17:59 +0530

    Populate data in input fields from table - 44

--- sixth_pro_41/resources/views/list-student.blade.php
+++ sixth_pro_41/resources/views/list-student.blade.php
@@ -6,7 +6,7 @@
             <td>Email</td>
             <td>Batch</td>
             <td>Created</td>
-            <td>Operation</td>
+            <td colspan="2">Operation</td>
         </tr>
         @foreach ($students as $stu)
         <tr>
@@ -15,6 +15,7 @@
             <td>{{ $stu->batch }}</td>
             <td>{{ $stu->created_at }}</td>
             <td><a href="delete-student/{{ $stu->id }}">Delete</a></td>
+            <td><a href="edit-student/{{ $stu->id }}">Edit</a></td>
         </tr>
         @endforeach
     </table>

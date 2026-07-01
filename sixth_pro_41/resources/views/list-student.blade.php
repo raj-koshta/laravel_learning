a4379347b47e1e25f63d8bf2203bd2fffe3a5915
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-07-01 23:49:11 +0530

    Delete Multiple record from mysql using destroy() function - 48

--- sixth_pro_41/resources/views/list-student.blade.php
+++ sixth_pro_41/resources/views/list-student.blade.php
@@ -1,34 +1,43 @@
 <style>
-    .w-5.h-5{
+    .w-5.h-5 {
         width: 20px;
     }
 </style>
 <div>
-    <h1>Student List</h1>
     <hr>
+    <h2>Search student</h2>
     <form action="search-student" method="get">
         <input type="text" name="search" placeholder="Search Via Name" value="{{ @$search }}">
         <input type="submit" value="Search">
     </form>
     <hr>
-    <table border="2">
-        <tr>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Batch</td>
-            <td>Created</td>
-            <td colspan="2">Operation</td>
-        </tr>
-        @foreach ($students as $stu)
-        <tr>
-            <td>{{ $stu->name }}</td>
-            <td>{{ $stu->email }}</td>
-            <td>{{ $stu->batch }}</td>
-            <td>{{ $stu->created_at }}</td>
-            <td><a href="delete-student/{{ $stu->id }}">Delete</a></td>
-            <td><a href="edit-student/{{ $stu->id }}">Edit</a></td>
-        </tr>
-        @endforeach
-    </table>
+    <form action="delete-multiple-students" method="post">
+        @csrf
+        
+        <h1>Student List</h1>
+        <hr>
+        <input type="submit" value="Delete Selected">
+        <table border="2">
+            <tr>
+                <td>Action</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Batch</td>
+                <td>Created</td>
+                <td colspan="2">Operation</td>
+            </tr>
+            @foreach ($students as $stu)
+                <tr>
+                    <td><input type="checkbox" name="ids[]" value="{{ $stu->id }}"></td>
+                    <td>{{ $stu->name }}</td>
+                    <td>{{ $stu->email }}</td>
+                    <td>{{ $stu->batch }}</td>
+                    <td>{{ $stu->created_at }}</td>
+                    <td><a href="delete-student/{{ $stu->id }}">Delete</a></td>
+                    <td><a href="edit-student/{{ $stu->id }}">Edit</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </form>
     {{ @$students->links() }}
 </div>

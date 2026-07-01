11d89c188eab0284b8cde9ceaef441e6d5518eb0
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-07-02 00:47:39 +0530

    Upload image in database - 49

---
+++ sixth_pro_41/resources/views/upload-image.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>Upload Image</h1>
+    <hr>
+    <form action="" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="file" id="" />
+        <input type="submit" value="Submit">
+    </form>
+</div>

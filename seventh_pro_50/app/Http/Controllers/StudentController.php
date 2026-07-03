dee9edba3766a944e422f41c06b193ba74fa4433
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-07-03 23:50:25 +0530

    Mutator - 59

--- seventh_pro_50/app/Http/Controllers/StudentController.php
+++ seventh_pro_50/app/Http/Controllers/StudentController.php
@@ -10,4 +10,17 @@ class StudentController extends Controller
     public function list(){
         return Student::all();
     }
+
+    public function save(){
+        $student = new Student();
+        $student->name = "Jonah";
+        $student->email = "Jonah@gm.co";
+        $student->batch = "2026";
+
+        if($student->save()){
+            return "New Student saved";
+        } else {
+            return "unable to save";
+        }
+    }
 }

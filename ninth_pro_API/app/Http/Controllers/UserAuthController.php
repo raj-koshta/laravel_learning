9345ee19118d9d76b788ebe967051360a361040c
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-07-07 01:17:19 +0530

    Authorization with Auth:sanctum

--- ninth_pro_API/app/Http/Controllers/UserAuthController.php
+++ ninth_pro_API/app/Http/Controllers/UserAuthController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
     public function login(Request $request){
-        return "login funtion";
+        $inputs =  $request->all();
+        $user = User::where('email', $inputs['email'])->first();
+        if(!$user || !Hash::check($inputs['password'],$user->password)){
+            return ['result' => "User Not found", 'status' => false];
+        }
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+        return ['result' => $success, 'status' => true];
     }
     public function signup(Request $request){
         $inputs =  $request->all();

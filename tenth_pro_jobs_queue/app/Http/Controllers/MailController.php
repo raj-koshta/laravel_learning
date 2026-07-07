13b2a9c9c76cc20cedc417106399976301ebd609
Raj_koshta <raj.koshta@mayuraconsultancy.com>
2026-07-08 00:48:49 +0530

    Send email with jobs and queue

--- tenth_pro_jobs_queue/app/Http/Controllers/MailController.php
+++ tenth_pro_jobs_queue/app/Http/Controllers/MailController.php
@@ -14,7 +14,10 @@ class MailController extends Controller
         $msg = $request->message;
         $subject = $request->subject;
 
-        dispatch(new SendWelcomeEmailJob($to, $msg, $subject))->delay(now()->addSeconds(5));
+        // dispatch(new SendWelcomeEmailJob($to, $msg, $subject))->delay(now()->addSeconds(5));
+        
+        // OR
+        SendWelcomeEmailJob::dispatch($to, $msg, $subject)->delay(now()->addSecond(5));
 
         return response()->json([
             'message' => 'Email has been queued successfully.'
